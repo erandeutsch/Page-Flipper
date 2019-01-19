d315c703206d31bf0ab7440274b3bb2f0fbc4ec6
erandeutsch <46551607+erandeutsch@users.noreply.github.com>
2019-01-19 15:04:09 -0800

    Debug main.py

--- SheetVision/main.py
+++ SheetVision/main.py
@@ -7,7 +7,7 @@ from best_fit import fit
 from rectangle import Rectangle
 from note import Note
 from random import randint
-from midiutil.MidiFile3 import MIDIFile
+from MidiFile3 import MIDIFile
 
 staff_files = [
     "resources/template/staff2.png", 
